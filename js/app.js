841c056ea56a2b3d837ee0d903cc87cc915f1867
Bridjet Mendyuk <bjmendy@Bridjets-MacBook-Pro.local>
2017-02-03 11:57:37 -0600

    added index and js file

---
+++ js/app.js
@@ -0,0 +1,190 @@
+console.log("hello world");
+	
+var test = "hey class!";
+
+// for(i = 0; i < 11; i++){
+// 	console.log(i);
+// 
+
+
+for(i = 1; i <= 10; i++);{
+ 	if(i % 2 === 0){
+		console.log(i + 'is even');
+	}
+
+
+for (i === 20; i === 0; i--){
+console.log(i)
+}
+
+for(i = 0; i < 11; i++){
+console.log(i);
+}
+
+var food =  ["yummy", "pizza", "tacos", "gyro"];
+food.push ("ice cream");
+for(i = 0; i <
+	food.length; i++){
+	console.log(food[i]);
+}
+
+var classmates = ["Bridjet", "Nate", "Ann", "Reuben"];
+console.log(classmates);
+for(i = 0; i < classmates.length; i++){
+	console.log(classmates[i]);
+}
+
+typeof someObject
+someObject.name = "Chris"
+someObject.height = 6
+for (prop in someObject){
+	console.log(someObject[prop]);
+}
+
+for(key in someObject){
+	console.log(someObject[key]);
+}
+
+// LOOP PRACTICE LOOP JS
+// practice below
+
+for (var i = 1; i <= 20; i++){
+	console.log(i)
+}
+
+for(var i = 1; i <= 20; i++){
+ 	if(i % 2 === 0){
+		console.log(i + ' is even');
+	}
+}
+var bagOfBeans = 50;
+
+for (var i = 100; i >= 0; i--){
+	if(i % 2 === 0) {
+
+		// console.log(bagOfBeans = i + bagOfBeans);
+		bagOfBeans = i + bagOfBeans
+	}
+}
+
+var mixedArray = ['cheese', 'pepperoni', 'sausage', 'coca cola', 'lemonade', 'dough', 'beer', 'wine', 'ranch', 'blue cheese', 'bbq sauce'];
+var ingredients = [];
+var drinks = [];
+var sauces = [];
+
+for (var i = 0; i < meal.length; i++){
+	console.log(meal[i])
+	if (meal[i] === 'cheese' || meal[i] === 'pepperoni')}
+		food.push(meal[i]);
+			}
+		else if(meal[i] === 'beer' || meal[i] === 'wine'){
+			drinks.push(meal[i]);
+			else {
+				sauces.push(meal[i]);
+		}
+}
+
+
+
+
+-----------------------------
+
+if (mixedArray[i] === [0, 1, 2, 5]){
+	ingredients.push(i);
+	console.log(ingredients);
+	} else if (mixedArray[i] === [3, 4, 6, 7]){
+		drinks.push(i);
+		console.log(drinks);
+	} else {
+		sauces.push(i);
+		console.log(sauces);
+	}
+
+
+for (var i in mixedArray){
+	if(i = 0, 1, 2, 5){
+		ingredients.push(i);
+		console.log(ingredients);
+	}
+	else if (i = 3, 4, 6, 7){
+		drinks.push(i);
+		console.log(drinks);
+	} else {
+		sauces.push(i);
+		console.log(sauces);
+	}
+}
+
+
+
+// Loop through the following array and push the items that are not numbers into a seperate array
+var mixedArray = [1, 2, 3, 'taco', 4, 5, 6, 'burrito', 7, 8, 9,
+'Whats the deal with airplane peanuts', 3.14, {barry: "Mannilow",}, 6, 55, null,
+"Old " + "Crow " + " Medicine Show", 3, 2, "I want the calzone from pizanos", "Death blow", "Firestorm", "48", 30]
+
+var coolArray = [];
+for (var i=)
+if (isNaN(mixedArray[i])) {
+	coolArray.push(i);
+	console.log("test")
+}
+---------------------------------
+
+var notANumber = []
+
+for (var i = o; i < mixedArray.length; i++){
+	if(typeof mixedArray[i] != "number"){
+		notaNumber.push(mixedArray[i])
+	}
+}
+
+notANumber[notANumber.length - 1]
+parseFloat('3.14')
+Math.floor(3.14)
+Math.ceil('3.14')
+
+// iterator=iterator + 1 <-----NOT THE SAME VALUE ---- how to assign a new value
+// iterator = iterator + 1 is the same at i++ 
+var lunch =["ciabatta", "tomato", "turkey", "cheese"];
+for (var iterator=0; iterator< lunch.length; iterator++)
+console.log(lunch[iterator]);
+
+-------------------------------------------
+
+
+// Use a for in loop to loop through the following objects and print to the console,
+
+// the name of each property.
+// var colorObjects = {red: 'red', blue: 'blue', cyan: 'cyan'}
+// var seinfeld = {cosmo: "kramer", jerry: "Seinfeld", george: "constanza", elaine: "bennis"}
+// var profile = {name: "Elon Musk", occupation: "space man", hobbies: ["paypal", "space X", "rocket science"]} 
+
+seinfeld.cosmo
+
+for (prop in seinfeld){
+	console.log(prop)
+	console.log(seinfeld [prop])
+}
+
+-------------------------------------------------
+data type javascript:
+string, number, undefined, boolean, object
+
+
+
+for (var i = 1; i< 5; i++) {
+	var str = ""
+	for (var j = 1; j <= i; j++){
+		str = str + "x";
+		console.log(str)
+	}
+};
+
+var pattern = [];
+for (var i = o; i <= 4; i++){
+	console.log(pattern += "x");
+}
+
+
+
+
